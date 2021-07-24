92d3c682902a81b470c893b09e0d71a97d2da6da
aragrevo <eduver_san@hotmail.com>
2021-07-24 14:04:55 -0500

    nav bar

--- src/app/feature/users/users.module.ts
+++ src/app/feature/users/users.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UsersRoutingModule } from './users-routing.module';
+
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeUserComponent } from './home-user/home-user.component';
 
 
 @NgModule({
-
+    declarations: [HomeUserComponent, NavBarComponent],
+    imports: [
+        CommonModule,
+        UsersRoutingModule,
+    ]
 })
 export class UsersModule {
 }
