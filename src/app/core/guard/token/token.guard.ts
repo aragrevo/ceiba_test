802bdba20ddf23a3a05f641c15801987b58beb99
aragrevo <eduver_san@hotmail.com>
2021-07-24 11:53:47 -0500

    token guard

--- src/app/core/guard/token/token.guard.ts
+++ src/app/core/guard/token/token.guard.ts
@@ -1,15 +1,25 @@
 import { Injectable } from '@angular/core';
-import {  ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree} from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TokenGuard implements CanActivate {
-  
+
+  constructor(private router: Router) { }
+
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
-    throw new Error('Method not implemented.');
+    const accessToken = this.getToken();
+    if (!!accessToken) return true;
+
+    this.router.navigate(['login'])
+    return false;
   }
-  
-  
+
+  private getToken(): string {
+    return localStorage.getItem('token');
+  }
+
+
 }
