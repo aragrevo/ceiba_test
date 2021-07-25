d7c45ddb6e0bf729ad701040e8e7452dbe05b20d
aragrevo <eduver_san@hotmail.com>
2021-07-25 08:38:04 -0500

    test

--- hacker-rank/test/user-list.spec.ts
+++ hacker-rank/test/user-list.spec.ts
@@ -74,6 +74,8 @@ describe('List users', () => {
     expect(avatar.tagName).toEqual('IMG');
     expect(btnDelete.tagName).toEqual('BUTTON');
     expect(email.textContent).toEqual(data[index].email);
+    console.log(email.textContent);
+
     expect(firstName.textContent).toEqual(data[index].first_name);
   }));
 
